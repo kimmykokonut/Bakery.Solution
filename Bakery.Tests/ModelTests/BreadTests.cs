04e157848f0b6b75724229554ef1cfdc1c75ee96
Kim Robinson <kimmykokonut@gmail.com>
2024-01-19 09:39:30 -0800

    pass test for bread auto-implemented property Order

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -8,8 +8,16 @@ namespace Bakery.Tests
     [TestMethod]
     public void BreadConstructor_CreatesInstanceOfBreadOrder_Bread()
     {
-      Bread newBread = new Bread();
+      Bread newBread = new Bread(1);
       Assert.AreEqual(typeof(Bread), newBread.GetType());
+    }
+    [TestMethod]
+    public void GetBreadOrder_ReturnsQuantity_Int()
+    {
+      int newOrder = 1;
+      Bread newBread = new Bread(newOrder);
+      int result = newBread.Order;
+      Assert.AreEqual(newOrder, result);
     }
   }
 }
--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -1,8 +1,13 @@
 namespace Bakery.Models
 {
-public class Bread
-{
-  //business logic code here
-}
+  public class Bread
+  {
+    public int Order { get; }
+    public Bread(int quantity)
+    {
+      Order = quantity;
+    }
+  }
 
 }
+// price set. ea x = 5 unless x%3, then x=x-1
